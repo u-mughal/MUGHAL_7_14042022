7860abb6751aaa23c6c47e39e41b95c244a521c5
Usman <usman.mughal@outlook.fr>
2022-05-23 19:16:17 +0200

    ajout fonctionnalité getComByUser

--- backend/controllers/Comments.js
+++ backend/controllers/Comments.js
@@ -2,7 +2,16 @@ import Comments from "../models/CommentModel.js"
 import Posts from "../models/PostModel.js";
 import Users from "../models/UserModel.js";
 
-export const getComByUser = async (req, res) => { }
+export const getComByUser = async (req, res) => {
+    try {
+        const comments = await Comments.findAll({
+            where: { userId: req.params.id }
+        });
+        res.json(comments);
+    } catch (error) {
+        res.json({ msg: error.msg });
+    }
+}
 
 
 export const getComByPost = async (req, res) => { }
