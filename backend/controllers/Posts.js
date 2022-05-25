282df2796cc53571d31e8d02137b5eb8806cefcd
Usman <usman.mughal@outlook.fr>
2022-05-25 18:38:39 +0200

    ajout fonctionnalité getAPost

--- backend/controllers/Posts.js
+++ backend/controllers/Posts.js
@@ -2,7 +2,17 @@ import Posts from "../models/PostModel.js";
 import Users from "../models/UserModel.js";
 import Comments from "../models/CommentModel.js";
 
-export const getAPost = async (req, res) => { }
+export const getAPost = async (req, res) => {
+    try {
+        const posts = await Posts.findAll({
+            include: [{ model: Users }],
+            where: { id: req.params.id }
+        });
+        res.json(posts);
+    } catch (error) {
+        res.json({ msg: error.msg });
+    }
+}
 
 
 export const getMyPosts = async (req, res) => { }
