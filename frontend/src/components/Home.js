702f479d0d524e77f659c44e3dd34ab978701398
Usman <usman.mughal@outlook.fr>
2022-05-25 20:34:47 +0200

    import des modules page home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,3 +1,16 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { useNavigate, NavLink, useLocation } from "react-router-dom";
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+import TimeAgo from 'timeago-react';
+import * as timeago from 'timeago.js';
+import fr from 'timeago.js/lib/lang/fr';
+timeago.register('fr', fr);
+
 
 const Dashboard = () => { }
 
